a800959c27ad47598b6796aa991f9b861494bc61
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-08-21 11:44:53 +0800

    [skip ci] Automated clean.

--- data_raw/results_mvn_ci.R
+++ data_raw/results_mvn_ci.R
@@ -11,12 +11,20 @@
 mvn_mcmvn_pcci <- readRDS("summary_medsimple_mvn_mcmvn_pcci.Rds")
 mvn_mct_pcci <- readRDS("summary_medsimple_mvn_mct_pcci.Rds")
 mvn_nb_bcaci <- readRDS("summary_medsimple_mvn_nb_bcaci.Rds")
+mvn_nb_bcci <- readRDS("summary_medsimple_mvn_nb_bcci.Rds")
+mvn_nb_pcci <- readRDS("summary_medsimple_mvn_nb_pcci.Rds")
 mvn_pbmvn_bcaci <- readRDS("summary_medsimple_mvn_pbmvn_bcaci.Rds")
+mvn_pbmvn_bcci <- readRDS("summary_medsimple_mvn_pbmvn_bcci.Rds")
+mvn_pbmvn_pcci <- readRDS("summary_medsimple_mvn_pbmvn_pcci.Rds")
 results_mvn_ci <- rbind(
   mvn_mcmvn_pcci,
   mvn_mct_pcci,
-  mvn_nb_bcaci,
-  mvn_pbmvn_bcaci
+  #  mvn_nb_bcaci,
+  #  mvn_nb_bcci,
+  mvn_nb_pcci,
+  #  mvn_pbmvn_bcaci,
+  #  mvn_pbmvn_bcci,
+  mvn_pbmvn_pcci
 )
 head(results_mvn_ci)
 str(results_mvn_ci)
